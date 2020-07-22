20da6bdbec37ff9240c7290c47d17bcfeb47d254
JhonSanz <ingjhonsanz@utp.edu.co>
2020-07-22 16:02:30 -0500

    heroku

--- src/app/components/mall/mall.component.ts
+++ src/app/components/mall/mall.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { MallService } from 'src/app/services/mall.service';
 import { Router } from '@angular/router';
-// import Swal from 'sweetalert2';
+import Swal from 'sweetalert2';
 
 
 @Component({
@@ -25,29 +25,29 @@ export class MallComponent implements OnInit {
   })
 
   public onCreateMall(data) {
-    // this.mallService.createMall(data).subscribe(
-    //   response => {
-    //     Swal.fire({
-    //       icon: 'success',
-    //       title: 'Datos correctos',
-    //       text: 'Tienda registrada con éxito',
-    //       onClose: () => location.reload()
-    //     });
-    //   }, error => {
-    //     switch (error.status) {
-    //       case 401:
-    //         this.router.navigateByUrl("/")
-    //         break
-    //       case 400:
-    //         Swal.fire({
-    //           icon: 'error',
-    //           title: 'Datos inválidos',
-    //           text: 'Something went wrong!',
-    //           onClose: () => location.reload()
-    //         });
-    //         break
-    //     }
-    //   }
-    // )
+    this.mallService.createMall(data).subscribe(
+      response => {
+        Swal.fire({
+          icon: 'success',
+          title: 'Datos correctos',
+          text: 'Tienda registrada con éxito',
+          onClose: () => location.reload()
+        });
+      }, error => {
+        switch (error.status) {
+          case 401:
+            this.router.navigateByUrl("/")
+            break
+          case 400:
+            Swal.fire({
+              icon: 'error',
+              title: 'Datos inválidos',
+              text: 'Something went wrong!',
+              onClose: () => location.reload()
+            });
+            break
+        }
+      }
+    )
   }
 }
--- src/app/components/visitor/visitor.component.ts
+++ src/app/components/visitor/visitor.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { VisitorService } from 'src/app/services/visitor.service'
 import { Router } from '@angular/router';
-// import Swal from 'sweetalert2';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-visitor',
@@ -25,30 +25,30 @@ export class VisitorComponent implements OnInit {
   })
 
   public onRegister(data) {
-    // this.visitorService.signupVisitor(data).subscribe(
-    //   response => {
-    //     Swal.fire({
-    //       icon: 'success',
-    //       title: 'Datos correctos',
-    //       text: 'Visitante registrado con éxito',
-    //       onClose: () => location.reload()
-    //     });
-    //   }, error => {
-    //     switch (error.status) {
-    //       case 401:
-    //         this.router.navigateByUrl("/")
-    //         break
-    //       case 400:
-    //         Swal.fire({
-    //           icon: 'error',
-    //           title: 'Datos inválidos',
-    //           text: 'Something went wrong!',
-    //           onClose: () => location.reload()
-    //         });
-    //         break
-    //     }
-    //   }
-    // )
+    this.visitorService.signupVisitor(data).subscribe(
+      response => {
+        Swal.fire({
+          icon: 'success',
+          title: 'Datos correctos',
+          text: 'Visitante registrado con éxito',
+          onClose: () => location.reload()
+        });
+      }, error => {
+        switch (error.status) {
+          case 401:
+            this.router.navigateByUrl("/")
+            break
+          case 400:
+            Swal.fire({
+              icon: 'error',
+              title: 'Datos inválidos',
+              text: 'Something went wrong!',
+              onClose: () => location.reload()
+            });
+            break
+        }
+      }
+    )
   }
 
 }
